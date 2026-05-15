9b82fd2f07aee427ccbacfef0e8eb14211c040b4
Mateusz Francik <mateusz.francik@swmansion.com>
2026-05-15 15:22:49 +0200

    fix(editor): Fixes for code review

--- editor/components/control-panel/components/PendingWhipInputs.tsx
+++ editor/components/control-panel/components/PendingWhipInputs.tsx
@@ -371,6 +371,7 @@ export function PendingWhipInputs({
       setActiveScreenshareInputId,
       setIsCameraActive,
       setIsScreenshareActive,
+      codecMap,
       setPendingInputs,
       updateInput,
       updateRoom,
